597cb994ca84bf9a231bcf65fdf3359d658d5b53
Gabe <popovici.gabriel@gmail.com>
2015-04-18 19:19:10 +0300

    add termination test case error

--- ResourceScheduler/src/test/java/com/jpmorgan/scheduler/ResourceSchedulerTest.java
+++ ResourceScheduler/src/test/java/com/jpmorgan/scheduler/ResourceSchedulerTest.java
@@ -2,14 +2,18 @@ package com.jpmorgan.scheduler;
 
 import com.jpmorgan.endpoint.Gateway;
 import com.jpmorgan.endpoint.GatewayMock;
+import com.jpmorgan.manager.ResourceError;
 import com.jpmorgan.manager.ResourceImpl;
 import com.jpmorgan.manager.ResourceManager;
 import com.jpmorgan.manager.ResourcePool;
 import com.jpmorgan.model.Group;
 import com.jpmorgan.model.MessageImpl;
+import org.junit.After;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
+
+import java.util.Arrays;
 
 /**
  * Class under test is ResourceScheduler
@@ -46,6 +50,19 @@ public class ResourceSchedulerTest {
         for (int i = 0; i < sequence.length; i++) {
             Assert.assertTrue(sequence[i].isCompleted());
         }
+    }
+
+    @Test(expected = TerminationMessageError.class)
+    public void shouldThrowErrorWhenMessageEnd() throws TerminationMessageError {
+        sequence = Arrays.copyOf(sequence, 4);
+        sequence[3] = MessageImpl.createSingleMessage("error", group1);
+        scheduler.sendSequence(sequence);
+    }
+
+
+    @After
+    public void release() throws ResourceError {
+        manager.stop();
     }
 
 }
