9e6d3a568ce16596c092cb41d9dd9d3ddd9eb4bd
Gabe <popovici.gabriel@gmail.com>
2018-11-06 01:21:05 +0200

    update project

--- coding-popovici/src/main/java/com/example/coding01/stock/ImportService.java
+++ coding-popovici/src/main/java/com/example/coding01/stock/ImportService.java
@@ -4,6 +4,8 @@ package com.example.coding01.stock;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.csv.CSVFormat;
 import org.apache.commons.csv.CSVParser;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.io.Resource;
 import org.springframework.stereotype.Service;
 
 import java.io.IOException;
@@ -15,10 +17,11 @@ import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 import java.util.LinkedList;
 import java.util.List;
-import java.util.Objects;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 import static java.lang.Float.parseFloat;
-import static java.util.stream.Collectors.toList;
+import static java.util.Objects.requireNonNull;
 
 @Service
 @Slf4j
@@ -26,43 +29,50 @@ public class ImportService {
 
     private final StockRepository stockRepository;
 
+    @Value("classpath:data")
+    private Resource resourceDir;
+
     public ImportService(StockRepository stockRepository) {
-        this.stockRepository = Objects.requireNonNull(stockRepository);
+        this.stockRepository = requireNonNull(stockRepository);
     }
 
     public void importData() throws IOException {
-        Path root = Paths.get("/Users/Gabe/projects/coding-popovici/src/main/resources/data/");
-        List<Path> csvPath = Files
-                .walk(root)
-                .filter(Files::isRegularFile)
-                .filter(path -> path.getFileName().toString().toUpperCase().endsWith("CSV"))
-                .collect(toList());
+        Path inputDirectory = Paths.get(resourceDir.getURI());
+        List<Path> csvPathFiles = getFiles(inputDirectory);
 
         final CSVFormat header = CSVFormat.RFC4180
                 .withHeader("Date", "Open", "High", "Low", "Close", "Adj Close", "Volume")
                 .withSkipHeaderRecord();
 
         List<Stock> stocks = new LinkedList<>();
-        csvPath.forEach(path -> {
+        csvPathFiles.forEach(path -> {
             try (Reader reader = Files.newBufferedReader(path);
                  CSVParser csvParser = new CSVParser(reader, header)) {
-                csvParser.forEach(record -> {
-                    record.get("Date");
-                    stocks.add(Stock.builder()
-                            .symbol(path.getFileName().toString().toUpperCase().replaceAll(".CSV", ""))
-                            .valueDate(LocalDate.parse(record.get("Date"), DateTimeFormatter.ISO_DATE))
-                            .open(parseFloat(record.get("Open")))
-                            .high(parseFloat(record.get("High")))
-                            .low(parseFloat(record.get("Low")))
-                            .close(parseFloat(record.get("Close")))
-                            .adjustedClose(parseFloat(record.get("Adj Close")))
-                            .volume(Integer.parseInt(record.get("Volume")))
-                            .build());
-                });
+                csvParser.forEach(record ->
+                        stocks.add(Stock.builder()
+                                .symbol(path.getFileName().toString().toUpperCase().replaceAll(".CSV", ""))
+                                .valueDate(LocalDate.parse(record.get("Date"), DateTimeFormatter.ISO_DATE))
+                                .open(parseFloat(record.get("Open")))
+                                .high(parseFloat(record.get("High")))
+                                .low(parseFloat(record.get("Low")))
+                                .close(parseFloat(record.get("Close")))
+                                .adjustedClose(parseFloat(record.get("Adj Close")))
+                                .volume(Integer.parseInt(record.get("Volume")))
+                                .build())
+                );
             } catch (IOException e) {
                 log.error("Import failed", e);
             }
             this.stockRepository.saveAll(stocks);
         });
+    }
+
+    private List<Path> getFiles(Path dataDirectory) throws IOException {
+        try (Stream<Path> rootStream = Files.walk(dataDirectory)) {
+            return rootStream
+                    .filter(Files::isRegularFile) // check for files
+                    .filter(path -> path.getFileName().toString().toUpperCase().endsWith("CSV")) // check specific files
+                    .collect(Collectors.toList()); // collect files
+        }
     }
 }
