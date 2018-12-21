84e194f7bea3fa534900ed0e4620cf3577e38243
Gabe <popovici.gabriel@gmail.com>
2018-12-21 21:39:21 +0200

    add more test cases and improve reporting

--- price-basket/run.cmd
+++ price-basket/run.cmd
@@ -1,2 +1,23 @@
-echo 'About to run PriceBasket items...'
+echo 'About to run PriceBasket'
+java -jar target/PriceBasket-0.1-SNAPSHOT.jar
+
+echo '------------------------------------------'
+echo '------------------------------------------'
+
+echo 'About to run PriceBasket Ferrari'
+java -jar target/PriceBasket-0.1-SNAPSHOT.jar Ferrari
+
+echo '------------------------------------------'
+echo '------------------------------------------'
+
+echo 'About to run PriceBasket Milk Soup'
+java -jar target/PriceBasket-0.1-SNAPSHOT.jar Milk Soup
+
+echo '------------------------------------------'
+echo '------------------------------------------'
+
+echo 'About to run PriceBasket Apples Milk Bread'
 java -jar target/PriceBasket-0.1-SNAPSHOT.jar Apples Milk Bread
+
+echo '------------------------------------------'
+echo '----------------THE END-------------------'
